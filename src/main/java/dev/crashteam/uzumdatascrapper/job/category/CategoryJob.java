46b2b865fb940b800204bd745b4b7bb01a69d2a6
SageFreke <lenigrast@gmail.com>
2023-07-04 22:42:07 +0300

    category job fix (#2)

--- src/main/java/dev/crashteam/uzumdatascrapper/job/category/CategoryJob.java
+++ src/main/java/dev/crashteam/uzumdatascrapper/job/category/CategoryJob.java
@@ -5,6 +5,7 @@ import dev.crashteam.uzumdatascrapper.exception.CategoryRequestException;
 import dev.crashteam.uzumdatascrapper.mapper.UzumCategoryToMessageMapper;
 import dev.crashteam.uzumdatascrapper.model.dto.UzumCategoryMessage;
 import dev.crashteam.uzumdatascrapper.model.uzum.UzumCategory;
+import dev.crashteam.uzumdatascrapper.model.uzum.UzumGQLResponse;
 import dev.crashteam.uzumdatascrapper.service.integration.UzumService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ import org.springframework.data.redis.connection.stream.RecordId;
 import org.springframework.retry.RetryCallback;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
@@ -47,29 +49,26 @@ public class CategoryJob implements Job {
     @Override
     @SneakyThrows
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        List<UzumCategory.Data> data = retryTemplate.execute((RetryCallback<List<UzumCategory.Data>, CategoryRequestException>) retryContext -> {
+        List<UzumCategory.Data> rootCategories = retryTemplate.execute((RetryCallback<List<UzumCategory.Data>, CategoryRequestException>) retryContext -> {
             var categoryData = uzumService.getRootCategories();
             if (categoryData == null) {
                 throw new CategoryRequestException();
             }
             return categoryData;
         });
-        for (UzumCategory.Data dataChild : data) {
-            postCategoryRecord(dataChild);
+        for (UzumCategory.Data childCategories : rootCategories) {
+            postCategoryRecord(childCategories);
         }
 
     }
 
     @SneakyThrows
-    private void postCategoryRecord(UzumCategory.Data data) {
-        UzumCategoryMessage categoryMessage = UzumCategoryToMessageMapper.categoryToMessage(data);
+    private void postCategoryRecord(UzumCategory.Data childCategory) {
+        UzumCategoryMessage categoryMessage = UzumCategoryToMessageMapper.categoryToMessage(childCategory);
         RecordId recordId = streamCommands.xAdd(streamKey.getBytes(StandardCharsets.UTF_8),
                 Collections.singletonMap("category".getBytes(StandardCharsets.UTF_8),
                         objectMapper.writeValueAsBytes(categoryMessage)));
         log.info("Posted [stream={}] category record with id - [{}]",
                 streamKey, recordId);
-        for (UzumCategory.Data child : data.getChildren()) {
-            postCategoryRecord(child);
-        }
     }
 }
