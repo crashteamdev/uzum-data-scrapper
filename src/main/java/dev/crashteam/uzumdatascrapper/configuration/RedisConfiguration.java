4920a296c35d337450510f136005c385fdac37e3
SageFreke <lenigrast@gmail.com>
2023-10-02 22:51:20 +0300

    Revert "add local redis config"
    This reverts commit 03bcbea26eaffc1ca87ba86afa47d8278690a6f0.

--- src/main/java/dev/crashteam/uzumdatascrapper/configuration/RedisConfiguration.java
+++ src/main/java/dev/crashteam/uzumdatascrapper/configuration/RedisConfiguration.java
@@ -1,7 +1,6 @@
 package dev.crashteam.uzumdatascrapper.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -20,12 +19,6 @@ public class RedisConfiguration {
 
     @Value("${spring.redis.password}")
     private String redisPassword;
-
-    @Bean
-    @ConditionalOnProperty(value = "redis.local", havingValue = "true")
-    public RedisStreamCommands streamLocalCommands(LettuceConnectionFactory redisConnectionFactory) {
-        return redisConnectionFactory.getConnection().streamCommands();
-    }
 
     @Bean
     public LettuceConnectionFactory lettuceConnectionFactory() {
